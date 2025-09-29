c9f934b23f1e76cebb6d74c0db60e0a9dad12dd1
Paglia20 <145615930+Paglia20@users.noreply.github.com>
2025-09-29 12:02:37 +0200

    ops

--- include/OLED.h
+++ include/OLED.h
@@ -8,3 +8,15 @@
 #define OLED_DC_PORT PORTD
 #define OLED_DC_DDR  DDRD
 #define OLED_DC_PIN  PD2   
+
+void oled_write_cmd1(uint8_t c);
+
+void oled_write_cmd2(uint8_t c, uint8_t a0);
+void oled_write_cmd3(uint8_t c, uint8_t a0, uint8_t a1);
+
+void oled_write_data(const uint8_t* p, uint16_t n);
+
+
+void OLED_init(void);
+
+void OLED_fill(uint8_t pattern);
