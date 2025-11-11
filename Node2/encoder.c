b258d61a7406668bd8a7e68293d903707c5c8141
Paglia20 <145615930+Paglia20@users.noreply.github.com>
2025-11-11 14:15:28 +0100

    finished

--- Node2/encoder.c
+++ Node2/encoder.c
@@ -4,15 +4,15 @@ static int32_t right_limit = 0;
 static int32_t left_limit  = 0;
 static int32_t MAX_COUNTS  = 0;
 
-#define DUTY_MAX      20000
+#define DUTY_MAX      14000
 #define I_ABS_MAX     15000.0f
 
 // Control state
 static volatile int32_t latest_setpoint = 0;   // written from CAN thread, read in ISR
 
 // PI gains & timing
-static const float Kp = 3.0f;
-static const float Ki = 0.10f;
+static const float Kp = 10.0f;
+static const float Ki = 1.0f;
 static const float Ts = 0.020f;                // 20 ms
 static float I = 0.0f;                     // integral accumulator
 
@@ -62,14 +62,14 @@ void encode_init(void) {
     //motor to right
     printf("cal right \n\r");
     motor_write(0, 0, 15000); //right movement
-    time_spinFor(msecs(1400));
+    time_spinFor(msecs(1100));
     right_limit = qdec_tc2_get_position();
 
 
     printf("cal left \n\r");
 
     motor_write(0, 1, 15000); //left movement
-    time_spinFor(msecs(1400));
+    time_spinFor(msecs(1100));
     left_limit = qdec_tc2_get_position();
 
     int32_t pos = qdec_tc2_get_position();
@@ -160,8 +160,8 @@ void update_motor(void)
     // Saturazione hard
     if (duty > DUTY_MAX) duty = DUTY_MAX;
 
-    // Soft stop in prossimità dei limiti meccanici
-    duty = taper_duty_near_limits(dir, duty, pos);
+    // // Soft stop in prossimità dei limiti meccanici
+    // duty = taper_duty_near_limits(dir, duty, pos);
 
     // Ulteriore safety: se già oltre i limiti, non spingere verso l’esterno
     if (pos >= left_limit  && dir == 1) duty = 0;  // a sinistra e chiedi più sinistra
@@ -173,10 +173,8 @@ void update_motor(void)
 
 void control_timer_init(void)
 {
-    // 1) Enable peripheral clock for TC0
     PMC->PMC_PCER0 |= (1 << ID_TC0);
 
-    // 2) Configure TC0 Channel 0 for compare match at 50 Hz
     TcChannel *ch = &TC0->TC_CHANNEL[0];
 
     ch->TC_CMR = 
@@ -184,23 +182,19 @@ void control_timer_init(void)
         TC_CMR_WAVE |                  // waveform mode
         TC_CMR_WAVSEL_UP_RC;           // reset on RC compare
 
-    // 3) RC = ticks per period = 656 250 Hz * 0.02 s = 13 125
     ch->TC_RC = 13125;
 
-    // 4) Enable interrupt on RC compare
     ch->TC_IER = TC_IER_CPCS;          // interrupt on RC match
     ch->TC_IDR = ~TC_IER_CPCS;
 
-    // 5) Enable NVIC interrupt
     NVIC_EnableIRQ(TC0_IRQn);
 
-    // 6) Start timer
     ch->TC_CCR = TC_CCR_CLKEN | TC_CCR_SWTRG;
 }
 
 void TC0_Handler(void)
 {
-    // mandatory: clear interrupt flag
+
     TC0->TC_CHANNEL[0].TC_SR;
     update_motor();
 }
