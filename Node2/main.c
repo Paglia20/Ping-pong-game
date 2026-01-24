f8efe5f1b05c043b5906376dc3da418a2885b7ec
Paglia20 <145615930+Paglia20@users.noreply.github.com>
2026-01-24 18:19:50 +0100

    Clean up comments in main.c
    Removed outdated comments regarding UART file inclusion and compilation errors.

--- Node2/main.c
+++ Node2/main.c
@@ -1,12 +1,3 @@
-/*
- * Remember to update the Makefile with the (relative) path to the uart.c file.
- * This starter code will not compile until the UART file has been included in the Makefile. 
- * If you get somewhat cryptic errors referencing functions such as _sbrk, 
- * _close_r, _write_r, _fstat etc, you have most likely not done that correctly.
-
- * If you get errors such as "arm-none-eabi-gcc: no such file", you may need to reinstall the arm gcc packages using
- * apt or your favorite package manager.
- */
 
 #include "main.h"
 
